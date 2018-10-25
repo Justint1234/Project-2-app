a1ea82dd8469b19cec82e3f2d91e562feaa73201
Justin Williams <justinwilliams@Justins-MacBook-Pro.local>
2018-10-25 15:54:25 -0400

    Still doing routes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const router = express.Router();
 const applicationController = require('../controllers/application')
 const martialartsController = require('../controllers/martialarts')
+const stylesController = require('../controllers/styles')
+const studentsController = require('../controllers/students')
+
+router.get('/', applicationController.index)
+
+router.get('martialarts', martialartsController.index)
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
