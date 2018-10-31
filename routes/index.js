f39cac4f6e0846283b449646f5fbff8e412a48f3
Justin Williams <justinwilliams@Justins-MacBook-Pro.local>
2018-10-31 08:58:19 -0400

    Finishing routes

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,6 @@ const applicationController = require('../controllers/application')
 const martialartsController = require('../controllers/martialarts')
 const stylesController = require('../controllers/styles')
 const studentsController = require('../controllers/students')
-// const weaponsController = require('../controllers/weapons')
-const historyController = require('../controllers/history')
 
 router.get('/', applicationController.index)
 
@@ -17,15 +15,9 @@ router.get('/students/new', studentsController.new)
 router.get('/students/:studentsId', studentsController.show )
 router.get('/styles',stylesController.index)
 router.get('/styles/new', stylesController.new)
+router.get('/styles/create', stylesController.create)
 router.get('/styles/:stylesId', stylesController.show)
 router.get(`/martialarts/:martialartsId/students/studentsId`, studentsController.show)
-// router.get('/weapons', weaponsController.index)
-// router.get('/weapons/new', weaponsController.new)
-// router.get('/weapons/:weaponsId', weaponsController.show)
-router.get('/history/', historyController.index)
-router.get('/history/new' , historyController.new )
-router.get('history/:historyId', historyController.show)
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
