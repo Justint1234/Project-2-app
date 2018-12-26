3b54ba6023e065373ceb5a9ad12f5457bbf4281d
Justin Williams <justinwilliams@Justins-MacBook-Pro.local>
2018-12-26 12:59:22 -0500

    Trying something

--- db/connections.js
+++ db/connections.js
@@ -1,22 +1,15 @@
-// const mongoose = require('mongoose');
-// mongoose.connect(process.env.MONGODB_URI).then(() => {
-//     console.log('Connected to MongoDB')
-// }) 
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log('Connected to MongoDB')
+}) 
 
-// mongoose.connection.once('open', () =>  {
-//     console.log(`Mongoose has connected to MongoDB`)
-// })
-// mongoose.connection.on('error', (error) => {
-//     console.error(`MongoDB conncection error!!! ${error}`)
-//     process.exit(-1)
-// })
-
-// module.exports = mongoose
-
-const mongoose = require('mongoose')
-
-mongoose.connect('mongodb://localhost/store-sample')
-
-
+mongoose.connection.once('open', () =>  {
+    console.log(`Mongoose has connected to MongoDB`)
+})
+mongoose.connection.on('error', (error) => {
+    console.error(`MongoDB conncection error!!! ${error}`)
+    process.exit(-1)
+})
 
 module.exports = mongoose
+
