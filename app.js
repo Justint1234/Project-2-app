2c9cce46b4c3ff3f3923d5fe060e1f58d7300a0a
Justin Williams <justinwilliams@Justins-MacBook-Pro.local>
2018-12-27 16:48:20 -0500

    Pushing

--- app.js
+++ app.js
@@ -27,6 +27,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
+
+
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
 //   next(createError(404));
